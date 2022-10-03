0b0026c761ba06f718e3cbd0ee0f836f8e48dc4d
Elmira Ilyasova <capricorn_26@mail.ru>
2022-10-03 14:48:49 +0700

    Add link to breadcrumbs

--- src/components/breadcrumbs/breadcrumbs.tsx
+++ src/components/breadcrumbs/breadcrumbs.tsx
@@ -1,13 +1,16 @@
+import { Link } from 'react-router-dom';
+import { AppUrl } from '../../consts';
+
 const Breadcrumbs = (): JSX.Element => (
   <div className="breadcrumbs">
     <div className="container">
       <ul className="breadcrumbs__list">
         <li className="breadcrumbs__item">
-          <a className="breadcrumbs__link" href="index.html">Главная
+          <Link className="breadcrumbs__link" to={AppUrl.Main}>Главная
             <svg width="5" height="8" aria-hidden="true">
               <use xlinkHref="#icon-arrow-mini"></use>
             </svg>
-          </a>
+          </Link>
         </li>
         <li className="breadcrumbs__item"><span className="breadcrumbs__link breadcrumbs__link--active">Каталог</span>
         </li>

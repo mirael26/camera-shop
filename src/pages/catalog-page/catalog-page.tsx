b7dba5f39e2e09a34331d67f4c3a20fd2f1936d0
Elmira Ilyasova <capricorn_26@mail.ru>
2022-10-03 14:48:55 +0700

    Add footer

--- src/pages/catalog-page/catalog-page.tsx
+++ src/pages/catalog-page/catalog-page.tsx
@@ -2,9 +2,10 @@ import Header from '../../components/header/header';
 import Promo from '../../components/promo/promo';
 import Breadcrumbs from '../../components/breadcrumbs/breadcrumbs';
 import Catalog from '../../components/catalog/catalog';
+import Footer from '../../components/footer/footer';
 
 const CatalogPage = ():JSX.Element => (
-  <>
+  <div className="wrapper">
     <Header />
     <main>
       <Promo />
@@ -13,7 +14,8 @@ const CatalogPage = ():JSX.Element => (
         <Catalog />
       </div>
     </main>
-  </>
+    <Footer />
+  </div>
 );
 
 export default CatalogPage;

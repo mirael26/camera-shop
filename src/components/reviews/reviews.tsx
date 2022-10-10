f041fab854cd87b2686b7f9a58ba3cab3efb1065
Elmira Ilyasova <capricorn_26@mail.ru>
2022-10-10 17:22:12 +0700

    Add showmore reviews button logic

--- src/components/reviews/reviews.tsx
+++ src/components/reviews/reviews.tsx
@@ -1,14 +1,18 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { useAppDispatch } from '../../hooks/useAppDispatch';
 import { useAppSelector } from '../../hooks/useAppSelector';
 import { loadReviews } from '../../store/api-action';
 import ReviewCard from './review-card/review-card';
 
-const Reviews = (): JSX.Element => {
+const DISPLAYED_COUNT_STEP = 3;
+
+const Reviews = (): JSX.Element | null => {
   const reviews = useAppSelector((state) => state.data.reviews);
   const dispatch = useAppDispatch();
   const { id } = useParams();
+
+  const [displayedCount, setDisplayedCount] = useState(DISPLAYED_COUNT_STEP);
 
   useEffect(() => {
     if (id) {
@@ -16,7 +20,10 @@ const Reviews = (): JSX.Element => {
     }
   }, [id]);
 
+  const displayedReviews = reviews ? reviews.slice(0, displayedCount) : null;
+
   return (
+    reviews &&
     <div className="page-content__section">
       <section className="review-block">
         <div className="container">
@@ -25,15 +32,16 @@ const Reviews = (): JSX.Element => {
             <button className="btn" type="button">Оставить свой отзыв</button>
           </div>
           <ul className="review-block__list">
-            {reviews && reviews.map((review, i) => {
+            {displayedReviews && displayedReviews.map((review, i) => {
               const key = `review-card-${i}`;
               return <ReviewCard key={key} review={review}/>;
             })}
           </ul>
-          <div className="review-block__buttons">
+          {reviews.length > displayedCount &&
+          <div className="review-block__buttons" onClick={() => setDisplayedCount(displayedCount + DISPLAYED_COUNT_STEP)}>
             <button className="btn btn--purple" type="button">Показать больше отзывов
             </button>
-          </div>
+          </div>}
         </div>
       </section>
     </div>

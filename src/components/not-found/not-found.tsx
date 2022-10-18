ca0dba0c4a1d545fcf0cbb4860497c832b0d25db
Elmira Ilyasova <capricorn_26@mail.ru>
2022-10-18 13:00:33 +0700

    Add tests for NotFound

--- src/components/not-found/not-found.tsx
+++ src/components/not-found/not-found.tsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom';
 import { AppUrl } from '../../consts';
 
 const NotFound = (): JSX.Element => (
-  <section className="not-found">
+  <section className="not-found" data-testid='not-found'>
     <div className="container">
       <h1 className="title title--h2 not-found__title">404 Not Found</h1>
       <Link className='btn btn--purple' to={AppUrl.Main}>Вернутся на главную</Link>

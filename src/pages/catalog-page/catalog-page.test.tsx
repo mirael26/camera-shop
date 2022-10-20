3b691a7c1f4139367567ae19d827a38f7f183941
Elmira Ilyasova <capricorn_26@mail.ru>
2022-10-21 00:49:48 +0700

    Refactor pages tests

--- src/pages/catalog-page/catalog-page.test.tsx
+++ src/pages/catalog-page/catalog-page.test.tsx
@@ -1,7 +1,14 @@
 import { renderWithReduxAndRouter } from '../../test/helpers/render-with-redux-and-router';
 import CatalogPage from './catalog-page';
 
-test('CatalogPage render correctly', () => {
-  const catalogPage = renderWithReduxAndRouter(<CatalogPage/>);
-  expect(catalogPage).toMatchSnapshot();
+jest.mock('../../components/header/header', () => 'Header');
+jest.mock('../../components/promo/promo', () => 'Promo');
+jest.mock('../../components/breadcrumbs/breadcrumbs', () => 'Breadcrumbs');
+jest.mock('../../components/catalog/catalog', () => 'Catalog');
+jest.mock('../../components/footer/footer', () => 'Footer');
+jest.mock('../../components/modal/add-to-cart-modal/add-to-cart-modal', () => 'AddToCartModal');
+
+test('CatalogPage renders correctly', () => {
+  const { asFragment } = renderWithReduxAndRouter(<CatalogPage/>);
+  expect(asFragment()).toMatchSnapshot();
 });
--- src/pages/product-page/product-page.test.tsx
+++ src/pages/product-page/product-page.test.tsx
@@ -1,7 +1,17 @@
 import { renderWithReduxAndRouter } from '../../test/helpers/render-with-redux-and-router';
 import ProductPage from './product-page';
 
+jest.mock('../../components/breadcrumbs/breadcrumbs', () => 'Breadcrumbs');
+jest.mock('../../components/footer/footer', () => 'Footer');
+jest.mock('../../components/header/header', () => 'Header');
+jest.mock('../../components/modal/add-to-cart-modal/add-to-cart-modal', () => 'AddToCartModal');
+jest.mock('../../components/modal/review-modal/review-modal', () => 'ReviewModal');
+jest.mock('../../components/reviews/reviews', () => 'Reviews');
+jest.mock('../../components/similar-products/similar-products', () => 'SimilarProducts');
+jest.mock('../../components/up-button/up-button', () => 'UpButton');
+jest.mock('../../components/product/product', () => 'Product');
+
 test('ProductPage render correctly', () => {
-  const productPage = renderWithReduxAndRouter(<ProductPage/>);
-  expect(productPage).toMatchSnapshot();
+  const { asFragment } = renderWithReduxAndRouter(<ProductPage/>);
+  expect(asFragment()).toMatchSnapshot();
 });

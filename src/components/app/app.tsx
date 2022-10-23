ec620b1ad18489d7e3abf13b5aa51a1712463a90
Elmira Ilyasova <capricorn_26@mail.ru>
2022-10-23 16:27:24 +0700

    Change hash-router to browser-router

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,14 +1,14 @@
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import ScrollToTop from '../scroll-to-top/scroll-to-top';
 import Redirect from '../redirect/redirect';
 import AppRouter from './app-router/app-router';
 
 const App = (): JSX.Element => (
-  <HashRouter>
+  <BrowserRouter>
     <ScrollToTop />
     <Redirect />
     <AppRouter />
-  </HashRouter>
+  </BrowserRouter>
 );
 
 export default App;

b772cf57f982e0608b226f1d4c6920fcfead1d41
Elmira Ilyasova <capricorn_26@mail.ru>
2022-10-31 15:58:42 +0700

    Correct scrollToTop exception

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -3,7 +3,7 @@ import useScrollToTop from '../../hooks/use-scroll-to-top';
 import Redirect from '../redirect/redirect';
 import AppRouter from './app-router/app-router';
 
-const SCROLL_TO_TOP_CATALOG_EXCEPTION = `${AppUrl.Catalog}${AppUrl.Page}`;
+const SCROLL_TO_TOP_CATALOG_EXCEPTION = `${AppUrl.Catalog}`;
 
 const App = (): JSX.Element => {
   useScrollToTop([SCROLL_TO_TOP_CATALOG_EXCEPTION]);

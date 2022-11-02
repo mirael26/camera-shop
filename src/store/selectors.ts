3ad47898baafc3493835810e4800bcf4250bedde
Elmira Ilyasova <capricorn_26@mail.ru>
2022-11-02 21:19:13 +0700

    Add selectors for min/max price

--- src/store/selectors.ts
+++ src/store/selectors.ts
@@ -1,8 +1,11 @@
+import { createSelector } from 'reselect';
 import { TRootReducer } from './store';
 
 export const getAllProducts = (state: TRootReducer) => state.data.products;
 export const getAllProductsCount = (state: TRootReducer) => state.data.products?.length;
 export const getDisplayedProducts = (state: TRootReducer) => state.data.displayedProducts;
+export const getMinPriceInDisplayed = (state: TRootReducer) => state.data.displayedProducts?.reduce((a, b) => a.price < b.price ? a : b).price;
+export const getMaxPriceInDisplayed = (state: TRootReducer) => state.data.displayedProducts?.reduce((a, b) => a.price > b.price ? a : b).price;
 export const getCurrentProduct = (state: TRootReducer) => state.data.currentProduct;
 export const getSimilarProducts = (state: TRootReducer) => state.data.similarProducts;
 export const getReviews = (state: TRootReducer) => state.data.reviews;
@@ -12,3 +15,13 @@ export const getAddingToCartItem = (state: TRootReducer) => state.view.addingToC
 export const getAddToCartModalStatus = (state: TRootReducer) => state.view.addToCartModalOpen;
 export const getReviewModalStatus = (state: TRootReducer) => state.view.reviewModalOpen;
 export const getRedirectPath = (state: TRootReducer) => state.view.redirect;
+
+export const getMinPriceInCatalog = createSelector(
+  getAllProducts,
+  (products) => products?.reduce((a, b) => a.price < b.price ? a : b).price
+);
+
+export const getMaxPriceInCatalog = createSelector(
+  getAllProducts,
+  (products) => products?.reduce((a, b) => a.price > b.price ? a : b).price
+);

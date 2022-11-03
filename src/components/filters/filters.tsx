829551a171a00bf2525c7d23e7a3fe6f0b033221
Elmira Ilyasova <capricorn_26@mail.ru>
2022-11-03 22:22:21 +0700

    Add Reset filters button

--- src/components/filters/filters.tsx
+++ src/components/filters/filters.tsx
@@ -1,20 +1,34 @@
+import { useSearchParams } from 'react-router-dom';
+import { Param } from '../../consts';
 import CategoryFilter from './category-fitler/category-fitler';
 import LevelFilter from './level-filter/level-filter';
 import PriceFilter from './price-filter/price-filter';
 import TypeFilter from './type-filter/type-filter';
 
-const Filters = (): JSX.Element => (
-  <div className="catalog-filter">
-    <form action="#">
-      <h2 className="visually-hidden">Фильтр</h2>
-      <PriceFilter/>
-      <CategoryFilter/>
-      <TypeFilter/>
-      <LevelFilter/>
-      <button className="btn catalog-filter__reset-btn" type="reset">Сбросить фильтры
-      </button>
-    </form>
-  </div>
-);
+const Filters = (): JSX.Element => {
+  const [params, setParams] = useSearchParams();
+
+  const handleResetButtonClick = () => {
+    params.delete(Param.PriceMin);
+    params.delete(Param.PriceMax);
+    params.delete(Param.Category);
+    params.delete(Param.Level);
+    params.delete(Param.Type);
+    setParams(params);
+  };
+
+  return (
+    <div className="catalog-filter">
+      <form action="#">
+        <h2 className="visually-hidden">Фильтр</h2>
+        <PriceFilter/>
+        <CategoryFilter/>
+        <TypeFilter/>
+        <LevelFilter/>
+        <button className="btn catalog-filter__reset-btn" type="reset" onClick={handleResetButtonClick}>Сбросить фильтры</button>
+      </form>
+    </div>
+  );
+};
 
 export default Filters;

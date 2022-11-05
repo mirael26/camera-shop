b07c149d5baddf48a67531b00a4761b030b14253
Elmira Ilyasova <capricorn_26@mail.ru>
2022-11-05 17:47:36 +0700

    Update utils test

--- src/utils.test.ts
+++ src/utils.test.ts
@@ -1,4 +1,5 @@
-import { addPriceSeparators } from './utils';
+import { URLSearchParams } from 'url';
+import { addPriceSeparators, checkFilters, deleteOneParam } from './utils';
 
 describe('addPriceSeparators function', () => {
   test('Return correct three-digit result', () => {
@@ -24,5 +25,54 @@ describe('addPriceSeparators function', () => {
   test('If negative value return undefined', () => {
     const priceResult = addPriceSeparators(-3);
     expect(priceResult).toBeUndefined();
+  });
+});
+
+describe('deleteOneParam function', () => {
+  test('deletes param correctly if only one deleting param', () => {
+    const params = new URLSearchParams('?sort=price&category=camera');
+    const result = deleteOneParam('category', 'camera', params);
+
+    expect(result.has('category')).not.toBe(true);
+    expect(result.has('sort')).toBe(true);
+  })
+
+  test('deletes param correctly if several same params', () => {
+    const params = new URLSearchParams('?sort=price&category=camera&category=video');
+    const result = deleteOneParam('category', 'camera', params);
+
+    const allCategoryParams = result.getAll('category');
+    expect(allCategoryParams).toHaveLength(1);
+    expect(allCategoryParams.includes('camera')).not.toBe(true);
+    expect(result.has('sort')).toBe(true);
+  })
+
+  test('behavior correctly if deliting param does not exist but another same params exists', () => {
+    const params = new URLSearchParams('?sort=price&category=video');
+    const result = deleteOneParam('category', 'camera', params);
+
+    expect(result.toString()).toBe('sort=price&category=video');
+  })
+});
+
+describe('checkFilters function', () => {
+  test('unchecked filter correctly', () => {
+    const mockState = {camera: true, video: true, lens: true};
+    const setStateSpy = jest.fn();
+
+    const params = new URLSearchParams('?category=camera&category=video');
+    checkFilters(params, mockState, setStateSpy ,'category');
+
+    expect(setStateSpy).toBeCalledTimes(1);
+  });
+
+  test('checked several filter correctly', () => {
+    const mockState = {camera: true, video: false, lens: false};
+    const setStateSpy = jest.fn();
+
+    const params = new URLSearchParams('?category=camera&category=video&category=lens');
+    checkFilters(params, mockState, setStateSpy ,'category');
+
+    expect(setStateSpy).toBeCalledTimes(2);
   });
 });

3e36f7b9c65d5dd23b9d05ac91e1b45629cf7b05
Elmira Ilyasova <capricorn_26@mail.ru>
2022-11-06 13:48:24 +0700

    Update App tests

--- src/components/app/app-router/app-router.test.tsx
+++ src/components/app/app-router/app-router.test.tsx
@@ -1,38 +1,47 @@
 import { screen } from '@testing-library/react';
-import { AppUrl, Tab } from '../../../consts';
-import { renderTestApp } from '../../../test/helpers/render-test-app';
+import { AppUrl } from '../../../consts';
 import { renderWithReduxAndRouter } from '../../../test/helpers/render-with-redux-and-router';
 import AppRouter from './app-router';
 
 jest.mock('../../../pages/catalog-page/catalog-page', () => () => (<div data-testid='catalog-page'></div>));
 jest.mock('../../../pages/product-page/product-page', () => () => (<div data-testid='product-page'></div>));
 jest.mock('../../../pages/not-found-page/not-found-page', () => () => (<div data-testid='not-found-page'></div>));
+jest.mock('../../../pages/server-unavailable-page/server-unavailable-page', () => () => (<div data-testid='server-unavailable-page'></div>));
 
 describe('Test AppRouter routers', () => {
-  test('Routes to catalog page', () => {
+  test('redirects from main to catalog page correctly', () => {
+    renderWithReduxAndRouter(<AppRouter/>, { route: AppUrl.Main });
+    const catalogPage = screen.getByTestId('catalog-page');
+    expect(catalogPage).toBeInTheDocument();
+  });
+
+  test('routes to catalog page', () => {
     renderWithReduxAndRouter(<AppRouter/>, { route: AppUrl.Catalog });
     const catalogPage = screen.getByTestId('catalog-page');
     expect(catalogPage).toBeInTheDocument();
-
-    renderTestApp(null, { route: `${AppUrl.Catalog}${AppUrl.Page}3` });
-    expect(catalogPage).toBeInTheDocument();
   });
 
-  test('Routes to product page', () => {
+  test('routes to product page', () => {
     renderWithReduxAndRouter(<AppRouter/>, { route: `${AppUrl.Catalog}${AppUrl.Product}/1` });
     const productPage = screen.getByTestId('product-page');
     expect(productPage).toBeInTheDocument();
-
-    renderWithReduxAndRouter(<AppRouter/>, { route: `${AppUrl.Catalog}${AppUrl.Product}/1/${Tab.Features}` });
-    expect(productPage).toBeInTheDocument();
   });
 
-  test('Routes to not-found page', () => {
+  test('routes to not-found page on route 404', () => {
     renderWithReduxAndRouter(<AppRouter/>, { route: AppUrl.NotFound });
     const notFoundPage = screen.getByTestId('not-found-page');
     expect(notFoundPage).toBeInTheDocument();
+  });
 
+  test('routes to not-found page on non-existen route', () => {
     renderWithReduxAndRouter(<AppRouter/>, { route: '/sdf57sdfsdf58' });
+    const notFoundPage = screen.getByTestId('not-found-page');
     expect(notFoundPage).toBeInTheDocument();
+  });
+
+  test('routes to server-unavailable page', () => {
+    renderWithReduxAndRouter(<AppRouter/>, { route: AppUrl.ServerUnavailable });
+    const serverUnavailablePage = screen.getByTestId('server-unavailable-page');
+    expect(serverUnavailablePage).toBeInTheDocument();
   });
 });
--- src/components/app/app.test.tsx
+++ src/components/app/app.test.tsx
@@ -1,6 +1,3 @@
-import { render } from '@testing-library/react';
-import { Provider } from 'react-redux';
-import { createReduxStore } from '../../store/store';
 import { renderWithReduxAndRouter } from '../../test/helpers/render-with-redux-and-router';
 import App from './app';
 

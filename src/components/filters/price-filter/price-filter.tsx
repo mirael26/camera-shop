9041ef4e08602d79a83b7610646b1651ce5cecbe
Elmira Ilyasova <capricorn_26@mail.ru>
2022-11-16 19:41:24 +0700

    Try to remove waste event listeners in PriceFilter

--- src/components/filters/price-filter/price-filter.tsx
+++ src/components/filters/price-filter/price-filter.tsx
@@ -39,102 +39,118 @@ const PriceFilter = () => {
     }
   }, [params]); // если добавить зависимости из eslint.warning, некорректно работает
 
+  const setMin = (evt: FocusEvent) => {
+    const value = (evt.target as HTMLInputElement).value;
+    console.log(value);
+    if (value === Param.PriceMin) { // если значение не изменилось
+      return;
+    }
+
+    if (value === '') { // если значение - пустая строка
+      params.delete(Param.PriceMin);
+      params.set(Param.Page, DEFAULT_PAGE);
+      setParams(params); // удаляем параметр
+      setInputValue((prev) => ({...prev, min: ''}));
+      return;
+    }
+
+    const max = params.get(Param.PriceMax) || catalogMax;
+    let newMin = value;
+
+    if (catalogMin && +value < catalogMin) {
+      newMin = catalogMin.toString(); // если мин.цена ниже мин.цены всех товаров - устанавливаем мин.существующую цену
+    } else if (max && +value > +max) {
+      newMin = max.toString(); // если мин.цена выше макс.цены, устанавливаем макс.цену
+    }
+
+    if (catalogAsc && !catalogAsc.find((product) => product.price === +newMin)) { // если в каталоге не найден товар с такой ценой
+      const nearestMinProduct = catalogAsc.find((product, i) => {
+        const prevProduct = catalogAsc[i - 1];
+        return prevProduct && (prevProduct.price < +newMin) && (product.price > +newMin); // находим товар с ближайшей минимальной ценой
+      });
+      const nearestMinPrice = nearestMinProduct?.price;
+      newMin = nearestMinPrice ? nearestMinPrice.toString() : newMin; // записываем цену этого товара
+    }
+
+    params.set(Param.PriceMin, newMin);
+    params.set(Param.Page, DEFAULT_PAGE);
+    setParams(params);
+    setInputValue((prev) => ({...prev, min: newMin})); // записываем новое значение в стейт
+
+    refMinInput.current?.removeEventListener('blur', setMin);
+    document.removeEventListener('keydown', handleMinEnterKeydown);
+  };
+
+  const handleMinEnterKeydown = (keydownEvt: KeyboardEvent) => {
+    if (keydownEvt.key === 'Enter') {
+      refMinInput.current?.blur();
+    }
+  };
+
   const handleMinInputChange = (evt: ChangeEvent<HTMLInputElement>) => {
     const value = evt.target.value;
     setInputValue((prev) => ({...prev, min: value})); // устанавливаем введенное значение в стейт для отображения
 
-    const setMin = () => {
-      if (value === '') { // если значение - пустая строка
-        params.delete(Param.PriceMin);
-        params.set(Param.Page, DEFAULT_PAGE);
-        setParams(params); // удаляем параметр
-        setInputValue((prev) => ({...prev, min: ''}));
-        return;
-      }
-
-      const max = params.get(Param.PriceMax) || catalogMax;
-      let newMin = value;
-
-      if (catalogMin && +value < catalogMin) {
-        newMin = catalogMin.toString(); // если мин.цена ниже мин.цены всех товаров - устанавливаем мин.существующую цену
-      } else if (max && +value > +max) {
-        newMin = max.toString(); // если мин.цена выше макс.цены, устанавливаем макс.цену
-      }
-
-      if (catalogAsc && !catalogAsc.find((product) => product.price === +newMin)) { // если в каталоге не найден товар с такой ценой
-        const nearestMinProduct = catalogAsc.find((product, i) => {
-          const prevProduct = catalogAsc[i - 1];
-          return prevProduct && (prevProduct.price < +newMin) && (product.price > +newMin); // находим товар с ближайшей минимальной ценой
-        });
-        const nearestMinPrice = nearestMinProduct?.price;
-        newMin = nearestMinPrice ? nearestMinPrice.toString() : newMin; // записываем цену этого товара
-      }
-
-      params.set(Param.PriceMin, newMin);
-      params.set(Param.Page, DEFAULT_PAGE);
-      setParams(params);
-      setInputValue((prev) => ({...prev, min: newMin})); // записываем новое значение в стейт
-
-      refMinInput.current?.removeEventListener('blur', setMin);
-      document.removeEventListener('keydown', handleMinEnterKeydown);
-    };
-
-    const handleMinEnterKeydown = (keydownEvt: KeyboardEvent) => {
-      if (keydownEvt.key === 'Enter') {
-        setMin();
-      }
-    };
-
+    refMinInput.current?.removeEventListener('blur', setMin);
+    document.removeEventListener('keydown', handleMinEnterKeydown);
     refMinInput.current?.addEventListener('blur', setMin);
     document.addEventListener('keydown', handleMinEnterKeydown);
   };
 
-  const handleMaxInputChange = (evt: ChangeEvent<HTMLInputElement>) => {
+  const setMax = (evt: FocusEvent) => {
+    const value = (evt.target as HTMLInputElement).value;
+    console.log(value);
+    if (value === Param.PriceMax) { // если значение не изменилось
+      return;
+    }
+
+    if (value === '') { // если значение - пустая строка
+      params.delete(Param.PriceMax); // удаляем параметр
+      params.set(Param.Page, DEFAULT_PAGE);
+      setParams(params);
+      setInputValue((prev) => ({...prev, max: ''}));
+      return;
+    }
+
+    const min = params.get(Param.PriceMin) || catalogMin;
+    let newMax = value;
+
+    if (catalogMax && +value > catalogMax) {
+      newMax = catalogMax.toString(); // если макс.цена выше макс.цены всех товаров - устанавливаем макс.существующую цену
+    } else if (min && +value < +min) {
+      newMax = min.toString(); // если макс.цена ниже мин.цены, устанавливаем мин.цену
+    }
+
+    if (catalogAsc && !catalogAsc.find((product) => product.price === +newMax)) { // если в каталоге не найден товар с такой ценой
+      const nearestMinProduct = catalogAsc.find((product, i) => {
+        const nextProduct = catalogAsc[i + 1];
+        return nextProduct && (product.price < +newMax) && (nextProduct.price > +newMax); // находим товар с ближайшей максимальной ценой
+      });
+      const nearestMinPrice = nearestMinProduct?.price;
+      newMax = nearestMinPrice ? nearestMinPrice.toString() : newMax; // записываем цену этого товара
+    }
+
+    params.set(Param.PriceMax, newMax); // записываем новое значение в параметры
+    params.set(Param.Page, DEFAULT_PAGE);
+    setParams(params);
+    setInputValue((prev) => ({...prev, max: newMax})); // записываем новое значение в стейт
+
+    refMaxInput.current?.removeEventListener('blur', setMax);
+    document.removeEventListener('keydown', handleMaxEnterKeydown);
+  };
+
+  const handleMaxEnterKeydown = (keydownEvt: KeyboardEvent) => {
+    if (keydownEvt.key === 'Enter') {
+      refMaxInput.current?.blur();
+    }
+  };
+
+  const handleMaxInputChange = (evt: ChangeEvent<HTMLInputElement>) => {  
     const value = evt.target.value;
     setInputValue((prev) => ({...prev, max: value})); // устанавливаем введенное значение в стейт для отображения
 
-    const setMax = () => {
-      if (value === '') { // если значение - пустая строка
-        params.delete(Param.PriceMax); // удаляем параметр
-        params.set(Param.Page, DEFAULT_PAGE);
-        setParams(params);
-        setInputValue((prev) => ({...prev, max: ''}));
-        return;
-      }
-
-      const min = params.get(Param.PriceMin) || catalogMin;
-      let newMax = value;
-
-      if (catalogMax && +value > catalogMax) {
-        newMax = catalogMax.toString(); // если макс.цена выше макс.цены всех товаров - устанавливаем макс.существующую цену
-      } else if (min && +value < +min) {
-        newMax = min.toString(); // если макс.цена ниже мин.цены, устанавливаем мин.цену
-      }
-
-      if (catalogAsc && !catalogAsc.find((product) => product.price === +newMax)) { // если в каталоге не найден товар с такой ценой
-        const nearestMinProduct = catalogAsc.find((product, i) => {
-          const nextProduct = catalogAsc[i + 1];
-          return nextProduct && (product.price < +newMax) && (nextProduct.price > +newMax); // находим товар с ближайшей максимальной ценой
-        });
-        const nearestMinPrice = nearestMinProduct?.price;
-        newMax = nearestMinPrice ? nearestMinPrice.toString() : newMax; // записываем цену этого товара
-      }
-
-      params.set(Param.PriceMax, newMax); // записываем новое значение в параметры
-      params.set(Param.Page, DEFAULT_PAGE);
-      setParams(params);
-      setInputValue((prev) => ({...prev, max: newMax})); // записываем новое значение в стейт
-
-      refMaxInput.current?.removeEventListener('blur', setMax);
-      document.removeEventListener('keydown', handleMaxEnterKeydown);
-    };
-
-    const handleMaxEnterKeydown = (keydownEvt: KeyboardEvent) => {
-      if (keydownEvt.key === 'Enter') {
-        setMax();
-      }
-    };
-
+    refMaxInput.current?.removeEventListener('blur', setMax); // обновляем обработчики событий
+    document.removeEventListener('keydown', handleMaxEnterKeydown);
     refMaxInput.current?.addEventListener('blur', setMax);
     document.addEventListener('keydown', handleMaxEnterKeydown);
   };

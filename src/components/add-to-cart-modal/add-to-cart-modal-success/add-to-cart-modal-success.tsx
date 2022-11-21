64358397fe9ea0ceb411b9bd04235515ce49b7e3
Elmira Ilyasova <capricorn_26@mail.ru>
2022-11-21 16:00:37 +0700

    Add success mode to AddToCartModal

--- src/components/add-to-cart-modal/add-to-cart-modal-success/add-to-cart-modal-success.tsx
+++ src/components/add-to-cart-modal/add-to-cart-modal-success/add-to-cart-modal-success.tsx
@@ -1,18 +1,39 @@
-const AddToCartModalSuccess = () => (
-  <>
-    <p className="title title--h4">Товар успешно добавлен в корзину</p>
-    <svg className="modal__icon" width="86" height="80" aria-hidden="true">
-      <use xlinkHref="#icon-success"></use>
-    </svg>
-    <div className="modal__buttons"><a className="btn btn--transparent modal__btn" href="#">Продолжить покупки</a>
-      <button className="btn btn--purple modal__btn modal__btn--fit-width">Перейти в корзину</button>
-    </div>
-    <button className="cross-btn" type="button" aria-label="Закрыть попап">
-      <svg width="10" height="10" aria-hidden="true">
-        <use xlinkHref="#icon-close"></use>
+import { useNavigate } from 'react-router-dom';
+import { AppUrl } from '../../../consts';
+import { useAppDispatch } from '../../../hooks/use-app-dispatch';
+import { ActionCreator } from '../../../store/action';
+
+const AddToCartModalSuccess = () => {
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+
+  const handleToCatalogButtonClick = () => {
+    dispatch(ActionCreator.CloseModal());
+    navigate(AppUrl.Catalog);
+  };
+
+  const handleToCartButtonClick = () => {
+    dispatch(ActionCreator.CloseModal());
+    navigate(AppUrl.Cart);
+  };
+
+  return(
+    <>
+      <p className="title title--h4">Товар успешно добавлен в корзину</p>
+      <svg className="modal__icon" width="86" height="80" aria-hidden="true">
+        <use xlinkHref="#icon-success"></use>
       </svg>
-    </button>
-  </>
-);
+      <div className="modal__buttons">
+        <button className="btn btn--transparent modal__btn" onClick={handleToCatalogButtonClick}>Продолжить покупки</button>
+        <button className="btn btn--purple modal__btn modal__btn--fit-width" onClick={handleToCartButtonClick}>Перейти в корзину</button>
+      </div>
+      <button className="cross-btn" type="button" aria-label="Закрыть попап" onClick={() => dispatch(ActionCreator.CloseModal())}>
+        <svg width="10" height="10" aria-hidden="true">
+          <use xlinkHref="#icon-close"></use>
+        </svg>
+      </button>
+    </>
+  );
+};
 
 export default AddToCartModalSuccess;
--- src/components/add-to-cart-modal/add-to-cart-modal.tsx
+++ src/components/add-to-cart-modal/add-to-cart-modal.tsx
@@ -1,50 +1,20 @@
-import { useSelector } from 'react-redux';
-import { useAppDispatch } from '../../hooks/use-app-dispatch';
-import { ActionCreator } from '../../store/action';
-import { getAddingToCartItem } from '../../store/selectors';
-import { addPriceSeparators } from '../../utils';
+import { useState } from 'react';
+import AddToCartModalCard from '../add-to-cart-modal-card/add-to-cart-modal-card';
+import AddToCartModalSuccess from './add-to-cart-modal-success/add-to-cart-modal-success';
+
+const AddToCartModalMode = {
+  Add: 'add',
+  Success: 'success',
+} as const;
 
 const AddToCartModal = (): JSX.Element | null => {
-  const product = useSelector(getAddingToCartItem);
-  const dispatch = useAppDispatch();
-
-  const adaptedPrice = product ? addPriceSeparators(product.price) : null;
+  const [mode, setMode] = useState<typeof AddToCartModalMode.Add | typeof AddToCartModalMode.Success>(AddToCartModalMode.Add);
 
   return (
-    product &&
-    <>
-      <p className="title title--h4">Добавить товар в корзину</p>
-      <div className="basket-item basket-item--short">
-        <div className="basket-item__img">
-          <picture>
-            <source type="image/webp" srcSet={`${product.previewImgWebp}, ${product.previewImgWebp2x} 2x`}/>
-            <img src={product.previewImg} srcSet={`${product.previewImg2x} 2x`} width="140" height="120" alt={`Фотоаппарат «${product.name}»`}/>
-          </picture>
-        </div>
-        <div className="basket-item__description">
-          <p className="basket-item__title">{product.category} {product.name}</p>
-          <ul className="basket-item__list">
-            <li className="basket-item__list-item"><span className="basket-item__article">Артикул:</span> <span className="basket-item__number">{product.vendorCode}</span>
-            </li>
-            <li className="basket-item__list-item">{product.category}</li>
-            <li className="basket-item__list-item">{product.level} уровень</li>
-          </ul>
-          <p className="basket-item__price"><span className="visually-hidden">Цена:</span>{adaptedPrice} ₽</p>
-        </div>
-      </div>
-      <div className="modal__buttons">
-        <button className="btn btn--purple modal__btn modal__btn--fit-width" type="button">
-          <svg width="24" height="16" aria-hidden="true">
-            <use xlinkHref="#icon-add-basket"></use>
-          </svg>Добавить в корзину
-        </button>
-      </div>
-      <button className="cross-btn" type="button" aria-label="Закрыть попап" onClick={() => dispatch(ActionCreator.CloseModal())}>
-        <svg width="10" height="10" aria-hidden="true">
-          <use xlinkHref="#icon-close"></use>
-        </svg>
-      </button>
-    </>
+    <div className={`modal__content${mode === AddToCartModalMode.Success ? ' modal__content--narrow' : ''}`}>
+      {mode === AddToCartModalMode.Add && <AddToCartModalCard onSuccess={() => setMode(AddToCartModalMode.Success)} />}
+      {mode === AddToCartModalMode.Success && <AddToCartModalSuccess/>}
+    </div>
   );
 };
 

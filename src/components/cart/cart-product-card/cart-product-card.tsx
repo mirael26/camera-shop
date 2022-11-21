4dcb65c7ac86b294fc2b8e1944aff5bbbbaa0032
Elmira Ilyasova <capricorn_26@mail.ru>
2022-11-21 20:49:48 +0700

    Add CartProductCard with inputs

---
+++ src/components/cart/cart-product-card/cart-product-card.tsx
@@ -0,0 +1,112 @@
+import { useRef, useState } from 'react';
+import { useAppDispatch } from '../../../hooks/use-app-dispatch';
+import { ActionCreator } from '../../../store/action';
+import { IProductInCart } from '../../../types/data.type';
+import { addPriceSeparators } from '../../../utils';
+
+const MIN_COUNT = 1;
+const MAX_COUNT = 99;
+
+interface ICartProductCardProps {
+  product: IProductInCart;
+}
+
+const CartProductCard = ({ product }: ICartProductCardProps) => {
+  const dispatch = useAppDispatch();
+
+  const [countValue, setCountValue] = useState(product.countInCart);
+  const refInput = useRef<HTMLInputElement>(null);
+
+  const setCount = (evt: FocusEvent) => {
+    const value = +(evt.target as HTMLInputElement).value;
+
+    if (value <= MIN_COUNT) {
+      setCountValue(MIN_COUNT);
+      dispatch(ActionCreator.ChangeProductCountInCart(product.id, MIN_COUNT));
+    } else
+    if (value >= MAX_COUNT) {
+      setCountValue(MAX_COUNT);
+      dispatch(ActionCreator.ChangeProductCountInCart(product.id, MAX_COUNT));
+    } else {
+      dispatch(ActionCreator.ChangeProductCountInCart(product.id, value));
+    }
+
+    refInput.current?.removeEventListener('blur', setCount);
+    document.removeEventListener('keydown', handleInputEnterKeydown);
+  };
+
+  const handleInputEnterKeydown = (keydownEvt: KeyboardEvent) => {
+    if (keydownEvt.key === 'Enter') {
+      refInput.current?.blur();
+    }
+  };
+
+  const handleInputFocus = () => {
+    refInput.current?.addEventListener('blur', setCount);
+    document.addEventListener('keydown', handleInputEnterKeydown);
+  };
+
+  const handleDecreaseButtonClick = () => {
+    setCountValue(product.countInCart - 1);
+    dispatch(ActionCreator.ChangeProductCountInCart(product.id, product.countInCart - 1));
+  };
+
+  const handleIncreaseButtonClick = () => {
+    setCountValue(product.countInCart + 1);
+    dispatch(ActionCreator.ChangeProductCountInCart(product.id, product.countInCart + 1));
+  };
+
+  return (
+    <li className="basket-item">
+      <div className="basket-item__img">
+        <picture>
+          <source type="image/webp" srcSet={`${product.previewImgWebp}, ${product.previewImgWebp2x} 2x`}/>
+          <img src={product.previewImg} srcSet={`${product.previewImg2x} 2x`} width="140" height="120" alt={product.name}/>
+        </picture>
+      </div>
+      <div className="basket-item__description">
+        <p className="basket-item__title">{product.name}</p>
+        <ul className="basket-item__list">
+          <li className="basket-item__list-item"><span className="basket-item__article">Артикул:</span> <span className="basket-item__number">{product.vendorCode}</span>
+          </li>
+          <li className="basket-item__list-item">{product.type} {product.category.toLowerCase()}</li>
+          <li className="basket-item__list-item">{product.level} уровень</li>
+        </ul>
+      </div>
+      <p className="basket-item__price"><span className="visually-hidden">Цена:</span>{addPriceSeparators(product.price)} ₽</p>
+      <div className="quantity">
+        <button className="btn-icon btn-icon--prev" aria-label="уменьшить количество товара"
+          disabled={product.countInCart === MIN_COUNT}
+          onClick={handleDecreaseButtonClick}
+        >
+          <svg width="7" height="12" aria-hidden="true">
+            <use xlinkHref="#icon-arrow"></use>
+          </svg>
+        </button>
+        <label className="visually-hidden" htmlFor="counter1"></label>
+        <input type="number" id="counter1" min="1" max="99" aria-label="количество товара"
+          ref={refInput}
+          value={countValue}
+          onChange={(evt) => setCountValue(+evt.target.value)}
+          onFocus={handleInputFocus}
+        />
+        <button className="btn-icon btn-icon--next" aria-label="увеличить количество товара"
+          disabled={product.countInCart === MAX_COUNT}
+          onClick={handleIncreaseButtonClick}
+        >
+          <svg width="7" height="12" aria-hidden="true">
+            <use xlinkHref="#icon-arrow"></use>
+          </svg>
+        </button>
+      </div>
+      <div className="basket-item__total-price"><span className="visually-hidden">Общая цена:</span>{addPriceSeparators(product.price * product.countInCart)} ₽</div>
+      <button className="cross-btn" type="button" aria-label="Удалить товар">
+        <svg width="10" height="10" aria-hidden="true">
+          <use xlinkHref="#icon-close"></use>
+        </svg>
+      </button>
+    </li>
+  );
+};
+
+export default CartProductCard;

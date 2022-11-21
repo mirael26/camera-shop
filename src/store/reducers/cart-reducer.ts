015e8d9c82c3e445b6335e04b0d92f637b5f4094
Elmira Ilyasova <capricorn_26@mail.ru>
2022-11-21 20:48:51 +0700

    Fix cart-reducer work

--- src/store/reducers/cart-reducer.ts
+++ src/store/reducers/cart-reducer.ts
@@ -12,7 +12,7 @@ export const cartReducer = (state = initialState, action: TCartAction): ICartRed
     case ActionType.AddProductToCart:
       return state.productsInCart.some((product) => product.id === action.payload.id) // проверяем, есть ли товар в корзине
         // если есть - увеличиваем его количество в корзине на 1
-        ? {...state, productsInCart: state.productsInCart.map((product) => (product.id === action.payload.id) ? {...product, count: product.countInCart + 1} : product) || null}
+        ? {...state, productsInCart: state.productsInCart.map((product) => (product.id === action.payload.id) ? {...product, countInCart: product.countInCart + 1} : product) || null}
         // если нет - добавляем товар с количеством 1
         : {...state, productsInCart: [...state.productsInCart, {...action.payload, countInCart: 1}]};
 
@@ -22,7 +22,7 @@ export const cartReducer = (state = initialState, action: TCartAction): ICartRed
     case ActionType.ChangeProductCountInCart:
       return {...state,
         productsInCart: state.productsInCart.map((product) => (product.id === action.payload.id)
-          ? {...product, count: action.payload.count}
+          ? {...product, countInCart: action.payload.count}
           : product)};
     default:
       return state;
